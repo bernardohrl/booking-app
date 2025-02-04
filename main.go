1b7d0e470851c55480102bd73411c2ec62aa1a41
Bernardo Henrique <bernardohrl.unb@gmail.com>
2025-02-04 18:57:24 -0300

    Defining variables

--- main.go
+++ main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
-	fmt.Println("hello")
+	conferenceName := "Go Conference"
+	const conferenceTickets uint = 50
+	var remainingTickets uint = 50
+
+	fmt.Println("wellcome to", conferenceName, ". Total tickets:", conferenceTickets, ". Total available:", remainingTickets)
+	fmt.Printf("wellcome to %s. Total tickets: %d. Total available: %d", conferenceName, conferenceTickets, remainingTickets)
+
+	var userName string
+	var userTickets int
+	userName = "Tom"
+	userTickets = 2
+	fmt.Printf("\n%s bougth %d tickets\n", userName, userTickets)
 }
